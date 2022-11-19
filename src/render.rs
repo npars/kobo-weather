927443af3209032029a0dc458781c8071d188b56
npars <neilparsons@gmail.com>
2022-11-18 20:39:14 -0400

    Initial dashboard layout

--- src/render.rs
+++ src/render.rs
@@ -5,11 +5,13 @@ use usvg::roxmltree::Document;
 use usvg::{FitTo, Options, Tree};
 
 const WEATHER_FONT: &[u8] = include_bytes!("../resources/weathericons-regular-webfont.ttf");
+const TEXT_FONT: &[u8] = include_bytes!("../resources/Inter-Regular.ttf");
 
 pub(crate) fn render(svg_doc: &Document) -> Result<Pixmap> {
     debug!("Rendering the dashboard");
     let mut opt = Options::default();
     opt.fontdb.load_font_data(WEATHER_FONT.to_vec());
+    opt.fontdb.load_font_data(TEXT_FONT.to_vec());
 
     let tree = Tree::from_xmltree(svg_doc, &opt.to_ref())?;
 
